00199a7c8364f86a8b0b6621ba69ad59238f4bea
FilipAxel <filip_j97@hotmail.com>
2024-11-11 21:16:07 +0100

    enable cors for development

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_EMV !== 'production') {
+    app.enableCors({ origin: 'http://localhost:3001', credentials: true });
+  }
+
   await app.listen(3000);
 }
 bootstrap();
