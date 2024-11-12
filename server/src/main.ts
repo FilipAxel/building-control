5e2b1d22c9de6142575061f4c929b19a457e961e
FilipAxel <filip_j97@hotmail.com>
2024-11-12 20:33:29 +0100

    install swagger for api docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Building Control')
+    .setDescription(
+      'Building control is about managing the temperature in your buildings',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   if (process.env.NODE_EMV !== 'production') {
     app.enableCors({ origin: 'http://localhost:3001', credentials: true });
